37d4482b588b38cd496de81cdfe75e90ad5bdae6
EC2 Default User <satorukonuma_0604@yahoo.co.jp>
2023-06-29 13:45:59 +0000

    usersコントローラー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :books, dependent: :destroy
+  
+  has_one_attached :profile_image
+  
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/142_refileDownloadImage.png')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
+    end
+    profile_image.variant(resize_to_limit: [100, 100]).processed
+  end
 end
