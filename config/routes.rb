07f8631459aee7f251a488af37012b48f2a413f2
EC2 Default User <satorukonuma_0604@yahoo.co.jp>
2023-06-30 20:27:27 +0000

    いいね機能とコメント機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
