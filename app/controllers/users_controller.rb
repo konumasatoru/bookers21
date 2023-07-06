577c39d8650c117814adc2897c53a50155192a0f
EC2 Default User <satorukonuma_0604@yahoo.co.jp>
2023-07-06 06:22:35 +0000

    bookersほぼ完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :is_matching_login_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
     @books = @user.books.page(params[:page])
+    @book = Book.new
+  end
+  
+  def index
+    @users = User.all
+    @book = Book.new
   end
 
   def edit
@@ -20,7 +27,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name,  :introduction, :profile_image)
   end
   
   def is_matching_login_user
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
